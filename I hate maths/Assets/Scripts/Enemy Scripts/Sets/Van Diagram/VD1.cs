f1890ac1dcd7a8d2b73af481a093b79047ca8872
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-02-10 14:57:24 +0530

    21th Commit..

--- I hate maths/Assets/Scripts/Enemy Scripts/Sets/Van Diagram/VD1.cs
+++ I hate maths/Assets/Scripts/Enemy Scripts/Sets/Van Diagram/VD1.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class VD1 : MonoBehaviour
 {
+    [SerializeField] private Vector2 target;
+    [SerializeField] private int eventType;
     [SerializeField] private int randomRotatePoint;
     [SerializeField] float movementSpeed;
     [SerializeField] float rotateAmount;
@@ -16,6 +18,7 @@ public class VD1 : MonoBehaviour
     [SerializeField] float timeToMoveToDifferentPos;
 
     [SerializeField] Transform[] RotatePoint;
+    private Transform bus;
 
     Rigidbody2D rb;
 
@@ -23,34 +26,72 @@ public class VD1 : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
 
+        bus = GameObject.FindGameObjectWithTag("DreamBus").transform;
+
+
         randomRotatePoint = Random.Range(0, RotatePoint.Length);
+        eventType = 1;
 
         timeToMove = timeToMoveToDifferentPos;
+        target = new Vector2(bus.position.x, bus.position.y);
     }
 
+    private void LateUpdate()
+    {
+        
+    }
     private void Update()
     {
         distance = Vector2.Distance(transform.position, RotatePoint[randomRotatePoint].position);
 
         //transform.RotateAround(RotatePoint[randomRotatePoint].position, Vector3.forward, -rotateAmount * Time.fixedDeltaTime);
 
-        if (distance > minDistance)
+        if(eventType == 1)
         {
-            transform.position = Vector2.MoveTowards(transform.position, RotatePoint[randomRotatePoint].position,
-            movementSpeed * Time.deltaTime);
-        }
-        else if (distance < minDistance)
-        {
-            transform.RotateAround(RotatePoint[randomRotatePoint].position, Vector3.forward, -rotateAmount * Time.deltaTime);
-            if (timeToMove <= 0)
+            if (distance > minDistance)
             {
-                randomRotatePoint = Random.Range(0, RotatePoint.Length);
-                timeToMove = timeToMoveToDifferentPos;
-            }else
-            {
-                timeToMove -= Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, RotatePoint[randomRotatePoint].position,
+                movementSpeed * Time.deltaTime);
             }
+            else if (distance < minDistance)
+            {
+                transform.RotateAround(RotatePoint[randomRotatePoint].position, Vector3.forward, -rotateAmount * Time.deltaTime);
+                if (timeToMove <= 0)
+                {
+                    randomRotatePoint = Random.Range(0, RotatePoint.Length);
+                    eventType = Random.Range(1, 3);
+                    if (eventType == 2)
+                    {
+                        target = bus.position;
+                    }
+                    timeToMove = timeToMoveToDifferentPos;
+                }
+                else
+                {
+                    timeToMove -= Time.deltaTime;
+                }
+            }
+        }else if(eventType == 2)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
+        }
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            eventType = 1;
+            randomRotatePoint = Random.Range(0, RotatePoint.Length);
+            return;
         }
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("DreamBus"))
+        {
+            eventType = 1;
+            randomRotatePoint = Random.Range(0, RotatePoint.Length);
+            return;
+        }
     }
 }
