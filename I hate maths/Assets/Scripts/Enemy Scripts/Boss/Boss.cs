03e291529d46d82d5b1f842b7958bb38653152ee
1tsTheLuffy <58876026+1tsTheLuffy@users.noreply.github.com>
2020-02-14 13:49:45 +0530

    36th Commit..

--- I hate maths/Assets/Scripts/Enemy Scripts/Boss/Boss.cs
+++ I hate maths/Assets/Scripts/Enemy Scripts/Boss/Boss.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    public int health;
+    public float health;
 
     private int randomMovePoint;
     [SerializeField] int eventType;
-
+    [SerializeReference] int secondStageEvent;
     [SerializeField] float movementSpeed; 
 
     [Header("Timers")]
@@ -27,13 +27,17 @@ public class Boss : MonoBehaviour
     [SerializeField] Transform[] shootPoint;
     [SerializeField] Transform bus;
 
+    [SerializeField] Color[] hitColor;
+
     Rigidbody2D rb;
+    SpriteRenderer sr;
     Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
 
         bus = GameObject.FindGameObjectWithTag("DreamBus").transform;
 
@@ -45,90 +49,141 @@ public class Boss : MonoBehaviour
         health = 50;
 
         eventType = Random.Range(1, 4);
+        secondStageEvent = Random.Range(1, 3);
         randomMovePoint = Random.Range(0, movePoints.Length);
     }
 
     private void Update()
     {
-        if(eventType == 1)
+        if (health > 25)
         {
-            animator.SetBool("isGun", true);
-            animator.SetBool("isThorn", false);
-
-            transform.Rotate(0f, 0f, 10f);
-
-            transform.position = Vector2.MoveTowards(transform.position, movePoints[randomMovePoint].position, movementSpeed * Time.deltaTime);
-
-            if(timer <= 0)
+            if (eventType == 1)
             {
-                for (int i = 0; i < 3; i++)
+                animator.SetBool("isGun", true);
+                animator.SetBool("isThorn", false);
+
+                transform.Rotate(0f, 0f, 10f);
+
+                transform.position = Vector2.MoveTowards(transform.position, movePoints[randomMovePoint].position, movementSpeed * Time.deltaTime);
+
+                if (timer <= 0)
                 {
-                    Instantiate(Bullet[i], shootPoint[i].position, shootPoint[i].rotation);
+                    shoot();
+                    timer = timeToShoot;
                 }
-                //Instantiate(Bullet[0], shootPoint[0].position, shootPoint[0].rotation);
-                //Instantiate(Bullet[1], shootPoint[1].position, shootPoint[1].rotation);
-                //Instantiate(Bullet[2], shootPoint[2].position, shootPoint[2].rotation);
+                else
+                {
+                    timer -= Time.deltaTime;
+                }
 
-                timer = timeToShoot;
-            }else
+                if (Vector2.Distance(transform.position, movePoints[randomMovePoint].position) < .2f)
+                {
+                    randomMovePoint = Random.Range(0, movePoints.Length);
+                }
+
+                if (startTime <= 0)
+                {
+                    eventType = Random.Range(1, 4);
+                    if (eventType == 2)
+                    {
+                        target = new Vector2(bus.position.x, bus.position.y);
+                    }
+                    startTime = timeToMoveToNextState;
+                }
+                else
+                {
+                    startTime -= Time.deltaTime;
+                }
+            }
+            else if (eventType == 2)
             {
-                timer -= Time.deltaTime;
+                animator.SetBool("isThorn", true);
+                animator.SetBool("isGun", false);
+                transform.Rotate(0f, 0f, 10f);
+
+                transform.position = Vector2.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
+            }
+            else if (eventType == 3)
+            {
+                animator.SetBool("isThorn", false);
+                animator.SetBool("isGun", false);
+
+                if (startTime <= 0)
+                {
+                    eventType = Random.Range(1, 4);
+                    if (eventType == 3)
+                    {
+                        eventType = 2;
+                    }
+                    startTime = timeToMoveToNextState;
+                }
+                else
+                {
+                    startTime -= Time.deltaTime;
+                }
             }
 
-            if(Vector2.Distance(transform.position, movePoints[randomMovePoint].position) < .2f)
+            if (transform.position.x == target.x && transform.position.y == target.y)
             {
-                randomMovePoint = Random.Range(0, movePoints.Length);               
-            }
-
-            if(startTime <= 0)
-            {
-                eventType = Random.Range(1, 4);
-                if(eventType == 2)
-                {
-                    target = new Vector2(bus.position.x, bus.position.y);
-                }
-                startTime = timeToMoveToNextState;
-            }else
-            {
-                startTime -= Time.deltaTime;
-            }
-        }else if(eventType == 2)
-        {
-            animator.SetBool("isThorn", true);
-            animator.SetBool("isGun", false);
-            transform.Rotate(0f, 0f, 10f);
-            
-            transform.position = Vector2.MoveTowards(transform.position, target, movementSpeed * Time.deltaTime);
-        }else if(eventType == 3)
-        {
-            animator.SetBool("isThorn", false);
-            animator.SetBool("isGun", false);
-
-            if(startTime <= 0)
-            {
-                eventType = Random.Range(1, 4);
-                if(eventType == 3)
-                {
-                    eventType = 2;
-                }
-                startTime = timeToMoveToNextState;
-            }else
-            {
-                startTime -= Time.deltaTime;
+                eventType = 2;
+                target = new Vector2(bus.position.x, bus.position.y);
+                //randomMovePoint = Random.Range(0, movePoints.Length);
+                return;
             }
         }
-
-        if(transform.position.x == target.x && transform.position.y == target.y)
+        if (health < 25)
         {
-            eventType = 2;
-            target = new Vector2(bus.position.x, bus.position.y);
-            //randomMovePoint = Random.Range(0, movePoints.Length);
-            return;
-        }
+            sr.color = Color.magenta;
+            animator.SetTrigger("Second");
 
-        if(health <= 0)
+            if(secondStageEvent == 1)
+            {
+                transform.Rotate(0f, 0f, 10f);
+
+             
+                if(timer <= 0)
+                {
+                    shoot();
+                    timer = .08f;
+                }else
+                {
+                    timer -= Time.deltaTime;
+                }
+
+                if(startTime <= 0)
+                {
+                    secondStageEvent = Random.Range(1, 3);
+                    startTime = timeToMoveToNextState;
+                }else
+                {
+                    startTime -= Time.deltaTime;
+                }
+            }else if(secondStageEvent == 2)
+            {
+                secondStageEvent = 1;
+                return;
+            }else if(secondStageEvent == 3)
+            {
+                health += .05f;
+                startTime = .5f;
+                if(startTime <= 0)
+                {
+                    secondStageEvent = Random.Range(1, 4);
+                    if(secondStageEvent == 3)
+                    {
+                        secondStageEvent = 2;
+                    }
+                    startTime = timeToMoveToNextState;
+                }else
+                {
+                    startTime -= timeToMoveToNextState;
+                }
+            }
+        } 
+
+        if(Input.GetKeyDown(KeyCode.L))
         {
-            Destroy(gameObject);
+            health = 24;
         }
     }
 
@@ -146,14 +201,35 @@ public class Boss : MonoBehaviour
             if(eventType == 3)
             {
                 Destroy(collision.transform.gameObject);
+                health -= 1; 
                 GameObject instance = Instantiate(damageParticle, collision.transform.position, Quaternion.identity);
                 Destroy(instance, 1.2f);
             }
             if(eventType == 2)
             {
                 Destroy(collision.transform.gameObject);
+                health -= 1;
                 eventType = 1;
             }
+            if (health < 25)
+            {
+                if (secondStageEvent == 1)
+                {
+                    Destroy(collision.transform.gameObject);
+                    health -= 1;
+                    GameObject instance = Instantiate(damageParticle, collision.transform.position, Quaternion.identity);
+                    Destroy(instance, 1.2f);
+                }
+            }
         }
+    }
+
+    private void shoot()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(Bullet[i], shootPoint[i].position, shootPoint[i].rotation);
+        }
+        Debug.Log("Shoot Called!!");
     }
 }
